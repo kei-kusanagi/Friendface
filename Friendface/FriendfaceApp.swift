30d90c4607bb31a658557686f9eddf746f6e6d00
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-10-23 17:15:09 -0600

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Juan Carlos Robledo Morales on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FriendfaceTests/FriendfaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceTests.swift
+//  FriendfaceTests
+//
+//  Created by Juan Carlos Robledo Morales on 23/10/24.
+//
+
+import Testing
+@testable import Friendface
+
+struct FriendfaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
